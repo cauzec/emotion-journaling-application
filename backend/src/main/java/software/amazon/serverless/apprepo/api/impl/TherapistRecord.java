2073c05b260cf5806144565fbe86f07cd3b54cdd
Raj Mishra <63592624+cauzec@users.noreply.github.com>
2024-01-20 14:36:15 +0530

    Src

---
+++ backend/src/main/java/software/amazon/serverless/apprepo/api/impl/TherapistRecord.java
@@ -0,0 +1,111 @@
+package software.amazon.serverless.apprepo.api.impl;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+/**
+ * TherapistRecord represents a record in Therapists DynamoDB table.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TherapistRecord {
+  public static final String USER_ID_ATTRIBUTE_NAME = "userId";
+  public static final String THERAPIST_ID_ATTRIBUTE_NAME = "therapistId";
+  public static final String CREATED_AT_ATTRIBUTE_NAME = "createdAt";
+  public static final String THERAPIST_NAME_ATTRIBUTE_NAME = "therapistName";
+  public static final String THERAPIST_AREA_ATTRIBUTE_NAME = "therapistArea";
+  public static final String THERAPIST_TYPE_ATTRIBUTE_NAME = "therapistType";
+  public static final String VERSION_ATTRIBUTE_NAME = "version";
+  public static final String THERAPIST_MOBILE_ATTRIBUTE_NAME = "therapistMob";
+
+  private String userId;
+  private String therapistId;
+  private Instant createdAt;
+  private String therapistName;
+  private String therapistArea;
+  private String therapistType;
+  private Long version;
+  private Long therapistMob;
+
+  /**
+   * Construct the record from a map of DynamoDB {@link AttributeValue}.
+   *
+   * @param record a map of DynamoDB {@link AttributeValue}
+   */
+  public TherapistRecord(final Map<String, AttributeValue> record) {
+    if (record.containsKey(USER_ID_ATTRIBUTE_NAME)) {
+      this.userId = record.get(USER_ID_ATTRIBUTE_NAME).s();
+    }
+    if (record.containsKey(THERAPIST_ID_ATTRIBUTE_NAME)) {
+      this.therapistId = record.get(THERAPIST_ID_ATTRIBUTE_NAME).s();
+    }
+    if (record.containsKey(THERAPIST_NAME_ATTRIBUTE_NAME)) {
+      this.therapistName = record.get(THERAPIST_NAME_ATTRIBUTE_NAME).s();
+    }
+    if (record.containsKey(CREATED_AT_ATTRIBUTE_NAME)) {
+      this.createdAt = Instant.parse(record.get(CREATED_AT_ATTRIBUTE_NAME).s());
+    }
+    if (record.containsKey(THERAPIST_TYPE_ATTRIBUTE_NAME)) {
+      this.therapistType = record.get(THERAPIST_TYPE_ATTRIBUTE_NAME).s();
+    }
+    if (record.containsKey(THERAPIST_AREA_ATTRIBUTE_NAME)) {
+      this.therapistArea = record.get(THERAPIST_AREA_ATTRIBUTE_NAME).s();
+    }
+    if (record.containsKey(THERAPIST_MOBILE_ATTRIBUTE_NAME)) {
+      this.therapistMob = Long.parseLong(record.get(THERAPIST_MOBILE_ATTRIBUTE_NAME).n());
+    }
+    if (record.containsKey(VERSION_ATTRIBUTE_NAME)) {
+      this.version = Long.parseLong(record.get(VERSION_ATTRIBUTE_NAME).n());
+    }
+  }
+
+  /**
+   * Convert TherapistRecord to a map of DynamoDB {@link AttributeValue}.
+   *
+   * @return a map of DynamoDB {@link AttributeValue}
+   */
+  public Map<String, AttributeValue> toAttributeMap() {
+    Map<String, AttributeValue> therapistMap = new HashMap<>();
+    if (userId != null) {
+      therapistMap.put(USER_ID_ATTRIBUTE_NAME, AttributeValue.builder().s(userId).build());
+    }
+    if (therapistId != null) {
+      therapistMap.put(THERAPIST_ID_ATTRIBUTE_NAME,
+            AttributeValue.builder().s(therapistId).build());
+    }
+    if (therapistName != null) {
+      therapistMap.put(THERAPIST_NAME_ATTRIBUTE_NAME,
+            AttributeValue.builder().s(therapistName).build());
+    }
+    if (createdAt != null) {
+      therapistMap.put(CREATED_AT_ATTRIBUTE_NAME,
+            AttributeValue.builder().s(createdAt.toString()).build());
+    }
+    if (therapistType != null) {
+      therapistMap.put(THERAPIST_TYPE_ATTRIBUTE_NAME,
+            AttributeValue.builder().s(therapistType).build());
+    }
+    if (therapistArea != null) {
+      therapistMap.put(THERAPIST_AREA_ATTRIBUTE_NAME,
+            AttributeValue.builder().s(therapistArea).build());
+    }
+    if (therapistMob != null) {
+      therapistMap.put(THERAPIST_MOBILE_ATTRIBUTE_NAME,
+            AttributeValue.builder().n(therapistMob.toString()).build());
+    }
+    if (version != null) {
+      therapistMap.put(VERSION_ATTRIBUTE_NAME,
+            AttributeValue.builder().n(version.toString()).build());
+    }
+    return therapistMap;
+  }
+}
---
+++ backend/src/main/java/software/amazon/serverless/apprepo/api/impl/TherapistService.java
@@ -0,0 +1,235 @@
+package software.amazon.serverless.apprepo.api.impl;
+
+import io.swagger.api.TherapistApi;
+import io.swagger.model.Therapist;
+import io.swagger.model.TherapistList;
+import io.swagger.model.TherapistSummary;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.inject.Inject;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.ConditionalCheckFailedException;
+import software.amazon.awssdk.services.dynamodb.model.DeleteItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.QueryRequest;
+import software.amazon.awssdk.services.dynamodb.model.QueryResponse;
+import software.amazon.awssdk.services.dynamodb.model.UpdateItemRequest;
+import software.amazon.serverless.apprepo.api.impl.pagination.InvalidTokenException;
+import software.amazon.serverless.apprepo.api.impl.pagination.TokenSerializer;
+import software.amazon.serverless.apprepo.container.config.ConfigProvider;
+
+/**
+ * ApplicationsService implements {@link TherapistApi}.
+ *
+ * <p>It interacts with DynamoDB when processing each API.
+ */
+@Slf4j
+@RequiredArgsConstructor
+public abstract class TherapistService implements TherapistApi {
+  static final Integer DEFAULT_LIST_THERAPISTS_LIMIT = 10;
+  private final TokenSerializer<Map<String, AttributeValue>> paginationTokenSerializer;
+  private final DynamoDbClient dynamodb;
+  private final ModelMapper modelMapper;
+  private final String tableName;
+  private final Clock clock;
+  @Context
+  @Setter
+  private SecurityContext securityContext;
+
+  @Inject
+  public TherapistService(
+        final TokenSerializer<Map<String, AttributeValue>> paginationTokenSerializer,
+        final DynamoDbClient dynamodb, final ConfigProvider configProvider) {
+    this(paginationTokenSerializer, dynamodb, configureModelMapper(),
+          configProvider.getTherapistsTableName(), Clock.systemUTC());
+  }
+
+  public TherapistService(
+        final TokenSerializer<Map<String, AttributeValue>> paginationTokenSerializer,
+        final DynamoDbClient dynamodb, final ConfigProvider configProvider,
+        final Clock clock) {
+    this(paginationTokenSerializer, dynamodb, configureModelMapper(),
+          configProvider.getTherapistsTableName(), clock);
+  }
+
+  private static ModelMapper configureModelMapper() {
+    ModelMapper modelMapper = new ModelMapper();
+    PropertyMap<TherapistRecord, Therapist> therapistMap =
+        new PropertyMap<TherapistRecord, Therapist>() {
+          protected void configure() {
+            map(source.getCreatedAt()).setCreationTime(null);
+          }
+        };
+
+    PropertyMap<TherapistRecord, TherapistSummary> therapistSummaryMap =
+        new PropertyMap<TherapistRecord, TherapistSummary>() {
+          protected void configure() {
+            map(source.getCreatedAt()).setCreationTime(null);
+          }
+        };
+
+    modelMapper.addMappings(therapistMap);
+    modelMapper.addMappings(therapistSummaryMap);
+    return modelMapper;
+  }
+
+  public Therapist addTherapist(final Therapist createTherapistInput) {
+    log.info("Creating therapist with input {}", createTherapistInput);
+    TherapistRecord therapistRecord = modelMapper.map(createTherapistInput,
+          TherapistRecord.class);
+    therapistRecord.setCreatedAt(Instant.now(clock));
+    therapistRecord.setVersion(1L);
+    therapistRecord.setUserId("Raj");
+    dynamodb.putItem(PutItemRequest.builder()
+            .tableName(tableName)
+            .item(therapistRecord.toAttributeMap())
+            .conditionExpression(
+                  String.format("attribute_not_exists(%s) AND attribute_not_exists(%s)",
+                        TherapistRecord.USER_ID_ATTRIBUTE_NAME,
+                        TherapistRecord.THERAPIST_ID_ATTRIBUTE_NAME))
+            .build());
+    return modelMapper.map(therapistRecord, Therapist.class);
+  }
+
+  public void deleteTherapist(final String therapistId) {
+    log.info("Deleting therapist {}", therapistId);
+    TherapistRecord therapistRecord = loadTherapist(therapistId);
+    Map<String, AttributeValue> expressionAttributeValues = new HashMap<>();
+    expressionAttributeValues.put(":v", AttributeValue.builder()
+          .n(therapistRecord.getVersion().toString())
+          .build());
+    dynamodb.deleteItem(DeleteItemRequest.builder()
+          .tableName(tableName)
+          .key(toKeyRecord(therapistId))
+          .conditionExpression(String.format("%s = :v", TherapistRecord.VERSION_ATTRIBUTE_NAME))
+          .expressionAttributeValues(expressionAttributeValues)
+          .build());
+  }
+
+  public Therapist getTherapist(String therapistId) {
+    log.info("Getting therapist {}", therapistId);
+    TherapistRecord therapistRecord = loadTherapist(therapistId);
+    return modelMapper.map(therapistRecord, Therapist.class);
+  }
+
+  public TherapistList listTherapist(final String nextToken, final Integer maxItems) {
+    log.info("Listing therapists with nextToken {} and maxItems {}", nextToken, maxItems);
+    Map<String, AttributeValue> expressionAttributeValues = new HashMap<>();
+    expressionAttributeValues.put(":u", AttributeValue.builder()
+          .s("Raj")
+          .build());
+
+    QueryRequest.Builder requestBuilder = QueryRequest.builder()
+          .consistentRead(true)
+          .tableName(tableName)
+          .keyConditionExpression(String.format("%s = :u",
+                TherapistRecord.USER_ID_ATTRIBUTE_NAME))
+          .expressionAttributeValues(expressionAttributeValues)
+          .limit(maxItems == null ? DEFAULT_LIST_THERAPISTS_LIMIT : maxItems);
+    if (nextToken != null) {
+      try {
+        requestBuilder.exclusiveStartKey(paginationTokenSerializer.deserialize(nextToken));
+      } catch (InvalidTokenException e){System.out.println(e);} 
+    }
+    QueryResponse queryResponse = dynamodb.query(requestBuilder.build());
+
+    List<TherapistSummary> therapistSummaries = queryResponse.items()
+          .stream()
+          .map(TherapistRecord::new)
+          .map(record -> modelMapper.map(record, TherapistSummary.class))
+          .collect(Collectors.toList());
+
+    TherapistList result = new TherapistList()
+          .therapists(therapistSummaries);
+    Map<String, AttributeValue> lastEvaluatedKey = queryResponse.lastEvaluatedKey();
+    if (lastEvaluatedKey != null && !lastEvaluatedKey.isEmpty()) {
+      result.nextToken(paginationTokenSerializer.serialize(lastEvaluatedKey));
+    }
+    return result;
+  }
+  
+  public Therapist updateTherapist(final Therapist updateTherapistInput,
+                                       final String therapistId) {
+    log.info("Updating therapist {} with input {}", therapistId, updateTherapistInput);
+
+    TherapistRecord therapistRecord = loadTherapist(therapistId);
+    Map<String, AttributeValue> expressionAttributeValues = new HashMap<>();
+    String therapistName = updateTherapistInput.getTherapistName();
+    String therapistType = updateTherapistInput.getTherapistType();
+    String therapistArea = updateTherapistInput.getTherapistArea();
+    List<String> updateExpressionList = new ArrayList<>();
+    if (therapistName != null) {
+      therapistRecord.setTherapistName(therapistName);
+      expressionAttributeValues.put(":a", AttributeValue.builder().s(therapistName).build());
+      updateExpressionList.add(String.format("%s = :a",
+            TherapistRecord.THERAPIST_NAME_ATTRIBUTE_NAME));
+    }
+    if (therapistType != null) {
+      therapistRecord.setTherapistType(therapistType);
+      expressionAttributeValues.put(":d", AttributeValue.builder().s(therapistType).build());
+      updateExpressionList.add(String.format("%s = :d",
+            TherapistRecord.THERAPIST_TYPE_ATTRIBUTE_NAME));
+    }
+    if (therapistArea != null) {
+      therapistRecord.setTherapistArea(therapistArea);
+      expressionAttributeValues.put(":h", AttributeValue.builder().s(therapistArea).build());
+      updateExpressionList.add(String.format("%s = :h",
+            TherapistRecord.THERAPIST_AREA_ATTRIBUTE_NAME));
+    }
+
+    long newVersion = therapistRecord.getVersion() + 1;
+    expressionAttributeValues.put(":nv", AttributeValue.builder()
+          .n(Long.toString(newVersion))
+          .build());
+    updateExpressionList.add(String.format("%s = :nv",
+          TherapistRecord.VERSION_ATTRIBUTE_NAME));
+    String updateExpression = String.format("SET %s", String.join(",", updateExpressionList));
+    expressionAttributeValues.put(":v", AttributeValue.builder()
+          .n(therapistRecord.getVersion().toString())
+          .build());
+    dynamodb.updateItem(UpdateItemRequest.builder()
+          .tableName(tableName)
+          .key(toKeyRecord(therapistId))
+          .updateExpression(updateExpression)
+          .expressionAttributeValues(expressionAttributeValues)
+          .conditionExpression(String.format("%s = :v", TherapistRecord.VERSION_ATTRIBUTE_NAME))
+          .build());
+    return modelMapper.map(therapistRecord, Therapist.class);
+  }
+
+  private TherapistRecord loadTherapist(final String therapistId) {
+    Map<String, AttributeValue> therapistMap = dynamodb.getItem(GetItemRequest.builder()
+          .tableName(tableName)
+          .consistentRead(Boolean.TRUE)
+          .key(toKeyRecord(therapistId))
+          .build()).item();
+    return new TherapistRecord(therapistMap);
+  }
+
+  private Map<String, AttributeValue> toKeyRecord(final String therapistId) {
+    return TherapistRecord.builder()
+          .userId("Raj")
+          .therapistId(therapistId)
+          .build()
+          .toAttributeMap();
+  }
+}
